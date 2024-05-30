81923fc2c6a9a76aa5003ac55700481bc797073e
yasssh9s <yashukla09@gmail.com>
2024-05-30 10:57:19 +0530

    Adding Cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Side from './components/Side'
+import Cards from './components/Cards'
 
 function App() {
   return (
     <div>
       <Navbar/>
       <Side/>
+      <Cards/>
     </div>
   )
 }
---
+++ src/components/Cards.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import './Cards.css'
+
+function Cards() {
+
+    const [data, setData] = useState(null);
+  useEffect(() => {
+    // Fetch data from the API
+    axios
+      .get("https://data.covid19india.org/v4/min/data.min.json")
+      .then((response) => {
+        console.log(response.data); // Log the data to inspect its structure
+        if (response.data) {
+          setData(response.data.TT);
+        }
+      })  //Catching Error while fetching data
+      .catch((error) => {
+        console.error("There was an error fetching the data!", error);
+      });
+  }, []);
+
+//   Check for data
+  if (!data) return <div>Loading...</div>;
+
+  // destructuring the data and assigning it to the variables
+  const totalCases = data?.total?.confirmed || 0;
+  const totalDeaths = data?.total?.deceased || 0;
+  const totalRecovered = data?.total?.recovered || 0;
+
+  return <div className="main">
+     <div className="">
+     <div className="cards">
+        <div className="card c">
+          <div className="card-header">
+          <h3>Cases</h3>
+          <span className="card-percentage">23% <span>↑</span></span>
+          </div>
+          <h1>{totalCases}</h1>
+        </div>
+        <div className="card r">
+        <div className="card-header">
+          <h3>Deaths</h3>
+          <span className="card-percentage">19% <span>↑</span></span>
+          </div>
+          <h1>{totalDeaths}</h1>
+        </div>
+        <div className="card g">
+        <div className="card-header">
+          <h3>Recovered</h3>
+          <span className="card-percentage">15% <span>↑</span></span>
+          </div>
+          <h1>{totalRecovered}</h1>
+        </div>
+      </div>
+
+     </div>
+
+  </div>;
+}
+
+export default Cards;
